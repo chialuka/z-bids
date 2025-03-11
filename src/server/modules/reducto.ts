7e90721a0daa3ccbf4009b8167943284405072a9
Chukwualuka Chiama <alukachiama@gmail.com>
2025-03-11 22:32:14 +0000

    remove extra try catch

--- src/server/modules/reducto.ts
+++ src/server/modules/reducto.ts
@@ -1,24 +1,21 @@
 import axios from "axios";
 
 export const parseFile = async ({ documentUrl }: { documentUrl: string }) => {
-  try {
-    console.log(process.env.REDUCTO_API_KEY, "the reducto api key", documentUrl);
-    const file = await axios({
-      url: "https://platform.reducto.ai/parse",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.REDUCTO_API_KEY}`,
-      },
-      data: {
-        document_url: "https://pa6rt2x38u.ufs.sh/f/4o4hdZi2cLBoNyUss4SEBa4kdGCJl1UK89s5XR0tASPiLoOm"
-      }
-    });
+	console.log(process.env.REDUCTO_API_KEY, "the reducto api key", documentUrl);
+	const file = await axios({
+		url: "https://platform.reducto.ai/parse",
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+			Authorization: `Bearer ${process.env.REDUCTO_API_KEY}`,
+		},
+		data: {
+			document_url:
+				"https://pa6rt2x38u.ufs.sh/f/4o4hdZi2cLBoNyUss4SEBa4kdGCJl1UK89s5XR0tASPiLoOm",
+		},
+	});
 
-    console.log({ file });
-    return file;
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
-}
+	console.log({ file });
+	return file;
+};
